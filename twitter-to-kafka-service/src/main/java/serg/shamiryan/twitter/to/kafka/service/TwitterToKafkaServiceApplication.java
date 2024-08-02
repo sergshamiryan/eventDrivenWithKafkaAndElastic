0130e13bb065a57845415450da5ceafbd6bf0d67
Sergey Shamiryan <sergshamiryan@gmail.com>
2024-08-02 16:32:39 +0400

    Twitter-keywords added

---
+++ twitter-to-kafka-service/src/main/java/serg/shamiryan/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,21 @@
+package serg.shamiryan.twitter.to.kafka.service;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(){
+        return args -> {
+            System.out.println("hello");
+        };
+    }
+}
