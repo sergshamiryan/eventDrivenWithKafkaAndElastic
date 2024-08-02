cd3fa2f4f1e0e85adf9620273f6316a21c9dc45b
Sergey Shamiryan <sergshamiryan@gmail.com>
2024-08-02 15:48:55 +0400

    Initial commit

---
+++ src/test/java/serg/shamiryan/eventdrivenwithkafkaandelastic/EventDrivenWithKafkaAndElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package serg.shamiryan.eventdrivenwithkafkaandelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenWithKafkaAndElasticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
