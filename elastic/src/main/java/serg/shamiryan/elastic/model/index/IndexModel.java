3466605e43cb133e5697f6b089626acb0e2cf047
Sergey Shamiryan <sergshamiryan@gmail.com>
2024-11-07 17:41:45 +0400

    adding elastic module

---
+++ elastic/src/main/java/serg/shamiryan/elastic/model/index/IndexModel.java
@@ -0,0 +1,13 @@
+package serg.shamiryan.elastic.model.index;
+
+/**
+ * Hello world!
+ *
+ */
+public interface IndexModel
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
